d3b2a1117f4e58f0236dff67ed479cd286535f96
deglan rivas <deglan.rivas@pucp.edu.pe>
2024-07-29 00:23:05 -0500

    feat: :sparkles: release cap7 done

--- apps/nest_pokedex_100/src/main.ts
+++ apps/nest_pokedex_100/src/main.ts
@@ -13,6 +13,18 @@ async function bootstrap() {
     }),
   );
 
+  // app.enableCors({
+  //   origin: 'http://localhost:3000', // O el dominio de tu frontend
+  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  //   allowedHeaders: 'Content-Type, Accept',
+  // });
+
   await app.listen(3000);
 }
 bootstrap();
+
+// para el  update del pokemon.service se puede retornar un Promise<Partial<Pokemon>> o inicializar instanciando la data en el constructor del Pokemon para retornar un obj o isntancia Pokemon o el toJSON() del pokemon escogido o mejor hacer que solo se busque por mongo id / pokemon id y quitamos los index cosa que así podemos hacer un findOne o findOneAndUpdate y comparamos las rows actualizadas para mandar un error si no se actualiza ninguna xd se sigue respetando el erro 11000 de paso -> esta última me parece la mejor, manqueé al permitir el id name y mongoid :'v
+// siempre tratar de hacer una sola consulta para no golpear tanto a la db, por eso prefiero pgsql a los orm y odm, aunque vi que prisma permite hacer raw queries
+// postman se loquea y devolvía error de cors, pero era solo que me había dado log out por inactividad xd
+
+// TODO para la próxima solo permitir un mongoid que debería tenerlo el componente de react que use un .forEach, así creamos un pipe en el backend de nest y golpeamos una sola vez a la mongodb en lugar de hasta 3 como hacemos ahora en el getOneById del .service.ts xd
--- apps/nest_pokedex_100/src/pokemon/pokemon.service.ts
+++ apps/nest_pokedex_100/src/pokemon/pokemon.service.ts
@@ -1,26 +1,88 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { isValidObjectId, Model } from 'mongoose';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
+import { Pokemon } from './entities/pokemon.entity';
 
 @Injectable()
 export class PokemonService {
-  create(createPokemonDto: CreatePokemonDto) {
-    return 'This action adds a new pokemon';
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+  ) { }
+
+  async create(createPokemonDto: CreatePokemonDto): Promise<Pokemon> {
+    try {
+      const pokemon = new this.pokemonModel(createPokemonDto);
+      return await pokemon.save();
+    } catch (error) {
+      this.handleException(error);
+    }
   }
 
   findAll() {
     return `This action returns all pokemon`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} pokemon`;
+  async findOne(term: string): Promise<Pokemon> {
+    // this.pokemonModel.findOne({ no: id });
+    // this.pokemonModel.findByIdAndUpdate
+
+    if (!isNaN(+term)) {
+      return await this.pokemonModel.findOne({ no: term });
+    }
+
+    if (isValidObjectId(term)) {
+      return await this.pokemonModel.findById(term);
+    }
+
+    const pokemon = await this.pokemonModel.findOne({ name: term });
+    if (!pokemon)
+      throw new BadRequestException(
+        `Pokemon with id, name or no "${term}" not found`,
+      );
+
+    return pokemon;
   }
 
-  update(id: number, updatePokemonDto: UpdatePokemonDto) {
-    return `This action updates a #${id} pokemon`;
+  async update(
+    term: string,
+    updatePokemonDto: UpdatePokemonDto,
+  ): Promise<Pokemon> {
+    const pokemon = await this.findOne(term);
+
+    try {
+      await pokemon.updateOne(updatePokemonDto, { new: true });
+
+      return {
+        ...pokemon.toJSON(),
+        ...updatePokemonDto,
+      };
+    } catch (error) {
+      this.handleException(error);
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} pokemon`;
+  async remove(id: string): Promise<void> {
+    const { deletedCount } = await this.pokemonModel.deleteOne({ _id: id });
+    if (deletedCount === 0)
+      throw new BadRequestException(`Pokemon with id ${id} not found`);
+  }
+
+  handleException(error: any) {
+    if (error.code === 11000) {
+      throw new BadRequestException(
+        'Pokemon already exists: ' + error.keyValue,
+      );
+    }
+    console.log(error);
+    throw new InternalServerErrorException(
+      "Can't create Pokemon - Check server logs",
+    );
   }
 }
